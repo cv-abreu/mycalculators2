a535894a8a8f676b6770df04cd03ede1d33a24e6
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2024-11-14 06:19:38 -0300

    up

---
+++ index.js
@@ -0,0 +1,30 @@
+   
+
+/*var num1 = Number(window.prompt("Escolha um número",""));
+
+var num2 = Number(window.prompt("escolha outro número",""));
+
+var operacao = window.prompt("qual operador: +  -  *  /","");
+
+
+if (operacao==="+"){
+
+    var resultado = num1+num2
+}
+
+if (operacao==="-"){
+
+    var resultado = num1-num2
+}
+
+if (operacao==="*"){
+
+    var resultado = num1*num2
+}
+
+if (operacao==="/"){
+
+    var resultado = num1/num2
+}
+
+alert(resultado);
